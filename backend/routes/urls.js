40906a74b8cf11f841b631be7515d7c031fbe10a
Maaz Saboowala <maazsaboowala07@gmail.com>
2024-07-05 18:45:57 +0530

    amazon top 5 done

--- backend/routes/urls.js
+++ backend/routes/urls.js
@@ -93,6 +93,100 @@ router.delete('/:id', async (req, res) => {
 
 
 
+// router.post('/ranking', async (req, res) => {
+//     const { title, keyword } = req.body;
+
+//     try {
+//         let page = 1;
+//         let found = false;
+//         let rank = -1;
+
+//         while (!found) {
+//             // Construct the search URL with the encoded keyword and current page number
+//             const searchUrl = `https://www.amazon.in/s?k=${encodeURIComponent(keyword)}&crid=UUQ4CAGPONAW&sprefix=${encodeURIComponent(keyword)}%2Caps%2C200&ref=nb_sb_noss_1&page=${page}`;
+
+//             // Make GET request to Amazon search URL
+//             const response = await axios.get(searchUrl, {
+//                 headers: {
+//                     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
+//                     "Accept-Language": "en-US,en;q=0.5",
+//                     "Referer": "https://www.amazon.com/",
+//                     "DNT": "1",
+//                     "Connection": "keep-alive"
+//                 }
+//             });
+
+//             // Check if the response status is 200 (OK)
+//             if (response.status === 200) {
+//                 const $ = cheerio.load(response.data);
+
+//                 let items;
+//                 let itemTitleSelector;
+
+//                 // Define potential selectors based on observed variations in Amazon's HTML structure
+//                 const potentialSelectors = [
+//                     'div.s-card-container',    // Example of an alternative selector, adjust as needed
+//                     'div.s-result-item'        // Common selector for search result items
+//                 ];
+
+//                 for (const selector of potentialSelectors) {
+//                     items = $(selector);
+
+//                     if (items.length > 0) {
+//                         itemTitleSelector = 'span.a-text-normal'; // Default item title selector, adjust if needed
+//                         break;
+//                     }
+//                 }
+
+//                 if (!itemTitleSelector) {
+//                     console.error('No suitable selector found for scraping Amazon');
+//                     return res.status(500).json({ message: 'Failed to fetch search results from Amazon' });
+//                 }
+
+//                 // Iterate over each item in the search results
+//                 items.each((index, element) => {
+//                     const itemTitleElement = $(element).find(itemTitleSelector);
+//                     const itemTitle = itemTitleElement.text().trim();
+//                     console.log(`Page ${page} - Item ${index + 1}: ${itemTitle}`);
+
+//                     // Check if the item title includes the desired product title
+//                     if (itemTitle.includes(title)) {
+//                         rank = (page - 1) * items.length + (index + 1); // Calculate rank considering pagination
+//                         found = true;
+//                         return false; // Exit the each loop
+//                     }
+//                 });
+
+//                 // Check if there are more pages using the provided selector
+//                 const nextPageExists = $('div.s-pagination-container').find('.s-pagination-next').length > 0;
+//                 if (!nextPageExists) {
+//                     break; // Exit the loop if no more pages
+//                 }
+
+//                 page++; // Increment page number for next request
+//             } else {
+//                 // Handle cases where Amazon does not respond with status 200
+//                 console.error(`Failed to fetch search results from Amazon`);
+//                 return res.status(500).json({ message: 'Failed to fetch search results from Amazon' });
+//             }
+//         }
+
+//         // If rank is found, respond with 200 and rank information
+//         if (rank !== -1) {
+//             res.status(200).json({ rank });
+//         } else {
+//             // If product not found, respond with 404
+//             res.status(404).json({ message: 'Product not found in search results' });
+//         }
+//     } catch (error) {
+//         // Handle any errors that occur during the request or parsing
+//         console.error('Error fetching Amazon search results:', error.message);
+//         res.status(500).json({ message: 'Error fetching Amazon search results' });
+//     }
+// });
+
+
+
 router.post('/ranking', async (req, res) => {
     const { title, keyword } = req.body;
 
@@ -100,12 +194,11 @@ router.post('/ranking', async (req, res) => {
         let page = 1;
         let found = false;
         let rank = -1;
+        let topProducts = [];
 
         while (!found) {
-            // Construct the search URL with the encoded keyword and current page number
             const searchUrl = `https://www.amazon.in/s?k=${encodeURIComponent(keyword)}&crid=UUQ4CAGPONAW&sprefix=${encodeURIComponent(keyword)}%2Caps%2C200&ref=nb_sb_noss_1&page=${page}`;
 
-            // Make GET request to Amazon search URL
             const response = await axios.get(searchUrl, {
                 headers: {
                     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
@@ -116,24 +209,21 @@ router.post('/ranking', async (req, res) => {
                 }
             });
 
-            // Check if the response status is 200 (OK)
             if (response.status === 200) {
                 const $ = cheerio.load(response.data);
 
                 let items;
                 let itemTitleSelector;
 
-                // Define potential selectors based on observed variations in Amazon's HTML structure
                 const potentialSelectors = [
-                    'div.s-card-container',    // Example of an alternative selector, adjust as needed
-                    'div.s-result-item'        // Common selector for search result items
+                    'div.s-card-container',
+                    'div.s-result-item'
                 ];
 
                 for (const selector of potentialSelectors) {
                     items = $(selector);
-
                     if (items.length > 0) {
-                        itemTitleSelector = 'span.a-text-normal'; // Default item title selector, adjust if needed
+                        itemTitleSelector = 'span.a-text-normal';
                         break;
                     }
                 }
@@ -143,47 +233,53 @@ router.post('/ranking', async (req, res) => {
                     return res.status(500).json({ message: 'Failed to fetch search results from Amazon' });
                 }
 
-                // Iterate over each item in the search results
                 items.each((index, element) => {
                     const itemTitleElement = $(element).find(itemTitleSelector);
                     const itemTitle = itemTitleElement.text().trim();
                     console.log(`Page ${page} - Item ${index + 1}: ${itemTitle}`);
 
-                    // Check if the item title includes the desired product title
+                    if (topProducts.length < 5) {
+                        const itemPrice = $(element).find('.a-price-whole').first().text().trim();
+                        const itemRating = $(element).find('.a-icon-alt').first().text().trim();
+                        topProducts.push({
+                            title: itemTitle,
+                            price: itemPrice,
+                            rating: itemRating
+                        });
+                    }
+
                     if (itemTitle.includes(title)) {
-                        rank = (page - 1) * items.length + (index + 1); // Calculate rank considering pagination
+                        rank = (page - 1) * items.length + (index + 1);
                         found = true;
                         return false; // Exit the each loop
                     }
                 });
 
-                // Check if there are more pages using the provided selector
+                if (found) break; // Exit the while loop if the product is found
+
                 const nextPageExists = $('div.s-pagination-container').find('.s-pagination-next').length > 0;
                 if (!nextPageExists) {
                     break; // Exit the loop if no more pages
                 }
 
-                page++; // Increment page number for next request
+                page++;
             } else {
-                // Handle cases where Amazon does not respond with status 200
                 console.error(`Failed to fetch search results from Amazon`);
                 return res.status(500).json({ message: 'Failed to fetch search results from Amazon' });
             }
         }
 
-        // If rank is found, respond with 200 and rank information
         if (rank !== -1) {
-            res.status(200).json({ rank });
+            res.status(200).json({ rank, topProducts });
         } else {
-            // If product not found, respond with 404
-            res.status(404).json({ message: 'Product not found in search results' });
+            res.status(404).json({ message: 'Product not found in search results', topProducts });
         }
     } catch (error) {
-        // Handle any errors that occur during the request or parsing
         console.error('Error fetching Amazon search results:', error.message);
         res.status(500).json({ message: 'Error fetching Amazon search results' });
     }
 });
+
 
 
 
--- frontend/react/src/components/AmazonModal.jsx
+++ frontend/react/src/components/AmazonModal.jsx
@@ -5,6 +5,7 @@ import { FiSearch, FiX } from 'react-icons/fi';
 const AmazonModal = ({ isOpen, onClose, title }) => {
   const [inputValue, setInputValue] = useState('');
   const [rank, setRank] = useState(null);
+  const [topProducts, setTopProducts] = useState([]);
   const [loading, setLoading] = useState(false);
 
   const handleSubmit = () => {
@@ -15,12 +16,14 @@ const AmazonModal = ({ isOpen, onClose, title }) => {
     };
     axios.post('http://localhost:3000/api/urls/ranking', data)
       .then(response => {
-        const receivedRank = response.data.rank;
-        setRank(receivedRank);
+        const { rank, topProducts } = response.data;
+        setRank(rank);
+        setTopProducts(topProducts || []);
       })
       .catch(error => {
         console.error('Error:', error);
         setRank('We are unable to track it');
+        setTopProducts([]);
       })
       .finally(() => {
         setLoading(false);
@@ -32,52 +35,83 @@ const AmazonModal = ({ isOpen, onClose, title }) => {
   return (
     <div className="fixed z-10 inset-0 overflow-y-auto bg-black bg-opacity-50 backdrop-blur-sm">
       <div className="flex items-center justify-center p-5 min-h-screen">
-        <div className="bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 w-full max-w-2xl mx-auto rounded-2xl shadow-2xl overflow-hidden border border-yellow-500">
-          <div className="relative px-8 py-10">
+        <div className="bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 w-full max-w-5xl mx-auto rounded-2xl shadow-2xl overflow-hidden border border-yellow-500">
+          <div className="relative p-8 flex flex-col md:flex-row">
             <button
               onClick={onClose}
-              className="absolute top-4 right-4 text-yellow-500 hover:text-yellow-400 transition duration-300  hover:bg-red-600 hover:text-white hover:rounded-3xl "
+              className="absolute top-4 right-4 text-yellow-500 hover:text-yellow-400 transition duration-300 hover:bg-red-600 hover:text-white hover:rounded-3xl"
             >
-              <FiX className="w-8 h-8 " />
+              <FiX className="w-8 h-8" />
             </button>
-            <h3 className="text-3xl font-bold text-yellow-500 mb-8 text-center " style={{ fontFamily:"Anton SC" ,fontWeight:"900"}}>Amazon Search Engine Rank</h3>
-            <div className="mb-8">
-              <div className="relative">
-                <h3 className='text-white mb-10 opacity-30' style={{ fontFamily: 'Roboto', fontWeight: 'normal' }}>Add a keyword here to get your product rank based on it </h3>
-                <h2 className='text-xl font-bold text-white mb-8 text-center' style={{fontFamily:"Wittgenstein", fontWeight:"700"}}>{title}</h2>
-                <input
-                  type="text"
-                  className="w-full py-4 px-6 bg-gray-700 text-white placeholder-gray-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow-500 transition duration-300"
-                  placeholder="Enter keyword here"
-                  value={inputValue}
-                  onChange={(e) => setInputValue(e.target.value)}
-                  style={{ fontFamily: 'Roboto', fontWeight: 'normal' }}
-                />
-                {/* <FiSearch className="absolute right-4 top-1/2 transform -translate-y-1/2 text-gray-400 w-5 h-5" /> */}
+            <div className="flex-1 pr-0 md:pr-8">
+              <h3 className="text-3xl font-bold text-yellow-500 mb-8 text-center" style={{ fontFamily: "Anton SC", fontWeight: "900" }}>
+                Amazon Search Engine Rank
+              </h3>
+              <div className="mb-8">
+                <div className="relative">
+                  <h3 className='text-white mb-10 opacity-30' style={{ fontFamily: 'Roboto', fontWeight: 'normal' }}>
+                    Add a keyword here to get your product rank based on it
+                  </h3>
+                  <h2 className='text-xl font-bold text-white mb-8 text-center' style={{ fontFamily: "Wittgenstein", fontWeight: "700" }}>
+                    {title}
+                  </h2>
+                  <input
+                    type="text"
+                    className="w-full py-4 px-6 bg-gray-700 text-white placeholder-gray-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow-500 transition duration-300"
+                    placeholder="Enter keyword here"
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)}
+                    style={{ fontFamily: 'Roboto', fontWeight: 'normal' }}
+                  />
+                </div>
+              </div>
+
+              {loading ? (
+                <div className="flex justify-center items-center py-8">
+                  <div className="w-24 h-24 border-4 border-yellow-500 border-t-transparent rounded-full animate-spin"></div>
+                </div>
+              ) : (
+                rank !== null && (
+                  <div className="bg-gray-700 rounded-lg p-6 mb-8">
+                    <h2 className="text-3xl font-bold text-yellow-500 text-center" style={{ fontFamily: "Wittgenstein", fontWeight: "700" }}>
+                      {rank === 'We are unable to track it' ? rank : `Rank: ${rank}`}
+                    </h2>
+                  </div>
+                )
+              )}
+              <div className="flex justify-center">
+                <button
+                  onClick={handleSubmit}
+                  className="px-8 py-4 bg-yellow-500 text-gray-900 rounded-lg hover:bg-yellow-400 transition duration-300 transform hover:scale-105 flex items-center"
+                  style={{ fontFamily: 'Roboto', fontWeight: 'bold' }}
+                >
+                  <FiSearch className="mr-2" />
+                  Check Rank
+                </button>
               </div>
             </div>
-            
-            {loading ? (
-              <div className="flex justify-center items-center py-8">
-                <div className="w-24 h-24 border-4 border-yellow-500 border-t-transparent rounded-full animate-spin"></div>
-              </div>
-            ) : rank !== null && (
-              <div className="bg-gray-700 rounded-lg p-6 mb-8">
-                <h2 className="text-3xl font-bold text-yellow-500 text-center" style={{fontFamily:"Wittgenstein", fontWeight:"700"}}>
-                  {rank === 'We are unable to track it' ? rank : `Rank: ${rank}`}
+            {Array.isArray(topProducts) && topProducts.length > 0 && (
+              <div className="flex-1 mt-8 md:mt-4 md:ml-8 bg-gray-700 rounded-lg p-6">
+                <h2 className="text-3xl font-bold text-yellow-500 text-center mb-6" style={{ fontFamily: "Wittgenstein", fontWeight: "700" }}>
+                  Top 5 Products
                 </h2>
+                <ul>
+                  {topProducts.map((product, index) => (
+                    <li key={index} className="mb-4">
+                      <h3 className="text-xl font-bold text-white" style={{ fontFamily: "Wittgenstein", fontWeight: "700" }}>
+                        {product.title}
+                      </h3>
+                      <p className="text-lg text-yellow-500" style={{ fontFamily: "Wittgenstein", fontWeight: "700" }}>
+                        {product.price}
+                      </p>
+                      <p className="text-lg text-yellow-500" style={{ fontFamily: "Wittgenstein", fontWeight: "700" }}>
+                        {product.rating}
+                      </p>
+                    </li>
+                  ))}
+                </ul>
               </div>
             )}
-            <div className="flex justify-center">
-              <button
-                onClick={handleSubmit}
-                className="px-8 py-4 bg-yellow-500 text-gray-900 rounded-lg hover:bg-yellow-400 transition duration-300 transform hover:scale-105 flex items-center"
-                style={{ fontFamily: 'Roboto', fontWeight: 'bold' }}
-              >
-                <FiSearch className="mr-2" />
-                Check Rank
-              </button>
-            </div>
           </div>
         </div>
       </div>
